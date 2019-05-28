c18591bbc86544cd8fe49354ae61d43a76f5b7db
Ciprian Popa <ciprian_popa2002@yahoo.com>
2019-05-28 21:18:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 function addElementToBeginningOfArray(array, begOfArray){
-  addElementToBeginningOfArray.slice(0, 1);
+  addElementToBeginningOfArray.slice(0);
 }
 return addElementToBeginningOfArray;
 
